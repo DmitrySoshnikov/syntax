773e4f627b6cf7a876f8422683be971c7c7e70f5
Dmitry Soshnikov <dmitry.soshnikov@gmail.com>
2015-11-26 22:04:30 -0800

    Added special-symbols

---
+++ src/special-symbols.js
@@ -0,0 +1,14 @@
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2015-present Dmitry Soshnikov <dmitry.soshnikov@gmail.com>
+ */
+
+/**
+ * Special "empty" symbol, Epsilon.
+ */
+export const EPSILON = 'ε';
+
+/**
+ * End of input, and bottom of the stack, "Dollar".
+ */
+export const EOF = '$';
