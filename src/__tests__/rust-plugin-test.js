361513e11d38e8559cfdb36846e6bda19c655012
Andrey Rublev <box@anru.me>
2019-05-30 07:37:40 +0200

    more accurate way for determining toolchan environment readiness for rust tests

--- src/__tests__/rust-plugin-test.js
+++ src/__tests__/rust-plugin-test.js
@@ -1,12 +1,53 @@
 import * as shelljs from 'shelljs';
 import path from 'path';
 
-const whichCargo = shelljs.which('cargo');
-const rustInstalled = whichCargo && whichCargo.code === 0;
+// rust-calc test package uses Rust 2018, minimum version for that rust edition is 1.31
+// https://blog.rust-lang.org/2018/12/06/Rust-1.31-and-rust-2018.html
+const rustVersionRequired = [1, 31];
 
+const whichCargo = shelljs.which('cargo');
+const whichMake = shelljs.which('make');
+const rustInstalled = whichCargo && whichCargo.code === 0;
+const makeInstalled = whichMake && whichMake.code === 0;
 const rustCalcDir = path.join(__dirname, 'rust-calc');
 
-if (rustInstalled) {
+function getRustVersion() {
+  const child = shelljs.exec('cargo --version');
+  if (child.code === 0) {
+    const semverRe = /\d+\.\d+\.\d+/;
+    const match = semverRe.exec(child.stdout);
+    if (match) {
+      return match[0].split('.').map(x => Number(x));
+    }
+  }
+  return [];
+}
+
+function semverGte(aNumbers, bNumbers) {
+  if (!aNumbers || !bNumbers) {
+    return false;
+  }
+  const maxLen = Math.max(aNumbers.length, bNumbers.length);
+  let eq = false;
+  for (let i = 0; i < maxLen; i++) {
+    const aNum = aNumbers[i];
+    const bNum = bNumbers[i];
+
+    if (aNum > bNum) {
+      return true;
+    } else if (aNum < bNum) {
+      return false;
+    } else {
+      eq = true;
+    }
+  }
+  return eq;
+}
+
+const rustVersion = getRustVersion();
+const minimumVersionSatisfied = semverGte(rustVersion, rustVersionRequired);
+
+if (makeInstalled && rustInstalled && minimumVersionSatisfied) {
   describe('rust plugin', () => {
     beforeAll(() => {
       shelljs.exec('make', {
@@ -15,12 +56,22 @@ if (rustInstalled) {
     }, 10000);
 
     it('calc rust example should build, also output must match expected value', () => {
-      const runResult = shelljs.exec('cargo run --quiet', {
+      let runResult = shelljs.exec('cargo run --quiet', {
         silent: true,
         cwd: rustCalcDir,
       });
 
-      expect(runResult.stderr).toEqual('');
+      if (runResult.code !== 0) {
+        // something went wrong, rerun command with full debug output
+        runResult = shelljs.exec('cargo run', {
+          silent: false,
+          cwd: rustCalcDir,
+        });
+        console.error(runResult.stderr);
+      } else {
+        expect(runResult.stderr).toEqual('');
+      }
+
       expect(runResult.code).toEqual(0);
       const stdout = runResult.stdout.toString('utf8');
 
@@ -33,7 +84,10 @@ if (rustInstalled) {
   describe('rust plugin mock', () => {
     it('noop', () => {
       console.warn(
-        'rust toolchain is not installed, tests for rust plugin will be skipped'
+        `make and rust toolchain version ${rustVersionRequired.join(
+          '.'
+        )} or greater are not installed.`,
+        `Tests for rust plugin will be skipped.`
       );
     });
   });
